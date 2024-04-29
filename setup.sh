6170eff18cc9ade58c12c00ef84e58ef73d792a8
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-04-29 19:38:02 -0300

    Add some comments

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 # testing='--dry-run'
 
-version="${0##*/} version 0.2"
+# version="${0##*/} version 0.2"
 
+# Import package list
 source ./packages
 
+# Run sync and upgrade
 echo -e 'Update system...\n'
 sudo xbps-install -Suy
 
@@ -16,6 +18,7 @@ else
     exit 1
 fi
 
+# Install some packages
 echo -e 'Install some packages...\n'
 sudo xbps-install -y $testing ${packages[@]}
 
@@ -26,6 +29,7 @@ else
     exit 1
 fi
 
+# Create folders and copy settings
 echo 'Configure system...'
 
 echo -e '\nCopy settings to .config...\n'
@@ -37,16 +41,19 @@ for f in home/*; do
     cp -r $f "$HOME/.${f##*/}"
 done
 
-echo -e 'Source bash_xw...\n'
-echo -e '\n[[ -f ~/.bash_xw ]] && source ~/.bash_xw' >> $HOME/.bashrc
-
 echo -e 'Copy bin folder...\n'
 mkdir -p $HOME/.bin
 cp -r bin/* $HOME/.bin/
 
+# Add custom prompt, shopts, alias, etc...
+echo -e 'Source bash_xw...\n'
+echo -e '\n[[ -f ~/.bash_xw ]] && source ~/.bash_xw' >> $HOME/.bashrc
+
+# Create user folders
 echo -e 'Create user folders...\n'
 xdg-user-dirs-update
 
+# Finish and reboot
 echo -e 'Setup complete...\n'
 read -p 'Press Enter to continue...' continues
 reboot
