1a0f316b785705cdecdd53856eca48cd457e9b49
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-12-08 10:58:06 -0300

    Modify script to only handle dotfiles

--- setup.sh
+++ setup.sh
@@ -1,117 +1,61 @@
 #!/bin/bash
 
-# Uncomment this line to testing mode in package manager
-# testing='--dry-run'
-
-# Package list
-source packages
-
-update_sys() {
-	echo -e 'Update system...\n'
-	sudo xbps-install -Suy $testing	
+set_keys() {
+    echo 'Keyboard setup...'
+    sudo mkdir -p /etc/X11/xorg.conf.d
+    sudo cp -ruv xorg.conf.d/* /etc/X11/xorg.conf.d/
 }
 
-install_pkgs() {
-	echo -e 'Install some packages...\n'
-	sudo xbps-install -y $testing ${packages[@]}
-}
-
-config_sys() {
-	echo 'Configure system...'
-	# Config keyboard and touchpad
-	echo 'Keyboard and touchpad setup...'
-	sudo mkdir -p /etc/X11/xorg.conf.d
-	sudo cp -ruv xorg.conf.d/* /etc/X11/xorg.conf.d/
-
-	# Create folders and copy settings
-	# ~/.config folder
+set_configs() {
+    echo 'Create ~/.config folder and copy settings'
 	echo 'Copy settings to .config...'
 	mkdir -p $HOME/.config
 	cp -ruv config/* $HOME/.config/
-
-	# Hidden files in HOME
-	echo 'Copy hidden files of home...'
-	for f in home/*; do
-		cp -ruv $f "$HOME/.${f##*/}"
-	done
-
-	# Folder to personal scripts
-	echo 'Copy bin folder...'
-	mkdir -p $HOME/bin
-	cp -ruv bin/* $HOME/bin/
-
-	# Add custom prompt, shopts, alias, etc...
-	echo 'Source bash_xw...'
-	if grep '~/.bash_xw' $HOME/.bashrc &> /dev/null; then
-		echo 'bash_xw is already set!'
-	else
-		echo -e '\n[[ -f ~/.bash_xw ]] && source ~/.bash_xw' >> $HOME/.bashrc
-	fi
-
-	# Change papirus-icon color
-	echo 'Set papirus-icon color...'
-	sudo papirus-folders -C indigo
-
-	# Create user folders
-	echo 'Create user folders...'
-	xdg-user-dirs-update
-
-	# Create .xinitrc
-	echo 'Create .xinitrc ...'
-	if [[  -f $HOME/.xinitrc ]] 2> /dev/null; then
-		echo 'There is already a .xinitrc'
-	else
-		echo 'exec bspwm' >> $HOME/.xinitrc
-	fi
-
-	# Set default wallpaper
-	# echo 'Set wallpaper...'
-	# feh --bg-scale wall015.jpg
 }
 
-post_installation() {
-	# Update system
-	update_sys
-	# If exit status equals success 0 OK! If not, exit with error status 1 
-	if [[ $? == 0 ]]; then
-		echo -e '\nComplete system update!\n'
-	else
-		echo -e '\nError! Exiting...\n'
-		exit 1
-	fi
-
-	# Installing some packages
-	install_pkgs
-	# If exit status equals success 0 OK! If not, exit with error status 1 
-	if [[ $? == 0 ]]; then
-		echo -e 'Complete package install!\n'
-	else
-		echo -e 'Error! Exiting...\n'
-		exit 1
-	fi
-
-	# Configure system
-	config_sys
-
-	# Finish and reboot
-	echo 'Setup complete...'
-	read -p 'Reboot system?[S/n]' continues
-	if [[ ${continues,,} != 'n' ]]; then
-		sudo reboot
-	fi
+set_home_hidden_files() {
+    echo 'Copy hidden files of home...'
+    for f in home/*; do
+        cp -ruv $f "$HOME/.${f##*/}"
+    done
 }
 
-instructions() {
+set_bin_folder() {
+    echo 'Copy bin folder...'
+    mkdir -p $HOME/bin
+    cp -ruv bin/* $HOME/bin/
+
+    echo 'Source bash_xw...'
+    if grep '~/.bash_xw' $HOME/.bashrc &> /dev/null; then
+        echo 'bash_xw is already set!'
+    else
+        echo -e '\n[[ -f ~/.bash_xw ]] && source ~/.bash_xw' >> $HOME/.bashrc
+    fi
+}
+
+config_sys() {
+    echo 'Configure system...'
+    set_keys
+    set_configs
+    set_home_hidden_files
+    set_bin_folder
+    set_bash_aliases
+}
+
+_help() {
 cat << EOF
 
 usage: ${0##*/} [flags]
 
   Options:
 
-    --install,-i       	Install packages from package list
-    --config-system,-c	Set system configs 
-    --post-install,-pi 	Complete setup
-    --help,-h          	Show this is message
+    --all,-a       	        Complete config system
+    --keys,-k       	    Set keyboard settings
+    --configs,-c	        Set ~/.config
+    --home,-ho 	            Set home hidden files
+    --src,-s                Set scripts folder
+    --bash-aliases,-bs      Source ~/.bash_aliases
+    --help,-h          	    Show this is message
 
 EOF
 }
@@ -119,9 +63,11 @@ EOF
 set -e
 
 case $@ in
-	--install|-i) install_pkgs;;
-	--config-system|-c) config_sys;;
-	--post-install|-pi) post_installation;;
-	--help|-h) instructions;;
-	*) echo 'Invalid parameter!' && instructions;;
+	--all|-a) config_sys;;
+	--keys|-k) set_keys;;
+	--configs|-c) set_configs;;
+	--home|-ho) set_home_hidden_files;;
+	--src|-s) set_bin_folder;;
+	--help|-h) _help;;
+	*) echo 'Invalid parameter!' && _help;;
 esac
